1ef814984318d9cbfe379889a99de1ea8abe4d26
jkchuong <jkchuong@gmail.com>
2023-10-29 10:44:13 +0000

    refactored code for triangle

--- src/main.cpp
+++ src/main.cpp
@@ -7,116 +7,96 @@
 
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader; // Vertex Array Object, Vertex Buffer Object
+unsigned int VAO, VBO, shader; // Vertex Array Object, Vertex Buffer Object
 
-// Vertex Shader
+// Vertex Shader - called per vertex. location is the index of the attribute we want to use
 static const char* vShader = "									\n\
-#version 330													\n\
+#version 330 core												\n\
 																\n\
-layout (location = 0) in vec3 pos;								\n\
+layout (location = 0) in vec4 position;							\n\
 																\n\
 void main()														\n\
 {																\n\
-	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
+	gl_Position = position;										\n\
 }";
 
-// Fragment Shader
+// Fragment Shader - called per pixel that needs to be rasterized
 static const char* fShader = "									\n\
-#version 330													\n\
+#version 330 core												\n\
 																\n\
-out vec4 color;													\n\
+layout (location = 0) out vec4 color;							\n\
 																\n\
 void main()														\n\
 {																\n\
 	color = vec4(1.0, 0.0, 0.0, 1.0);							\n\
 }";
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, NULL);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+		// Create char array on stack instead of heap
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	// Delete shaders when it detaches from the program
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 void CreateTriangle()
 {
 	// Coords of the three points of the triangle
-	GLfloat vertices[] = {
-		-1.0f, -1.0f, 0.0f, 
-		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f
+	float positions[] = {
+		-0.5f, -0.5f, 
+		 0.5f, -0.5f,
+		 0.0f,  0.5f,
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
-
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Static draw -> Not going to change triangle values
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // Static draw -> Not going to change triangle values
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbinding VBO
-
-	glBindVertexArray(0); // Unbinding the VAO
-
-}
-
-void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
-{
-	GLuint theShader = glCreateShader(shaderType);
-
-	const GLchar* theCode[1];
-	theCode[0] = shaderCode;
-
-	GLint codeLength[1];
-	codeLength[0] = strlen(shaderCode);
-
-	glShaderSource(theShader, 1, theCode, codeLength);
-	glCompileShader(theShader);
-
-	// Error checking
-	GLint result = 0;
-	GLchar eLog[1024] = { 0 };
-
-	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetProgramInfoLog(theShader, sizeof(eLog), NULL, eLog);
-		printf(" Error compiling the %d shader: '%s'\n", shaderType, eLog);
-		return;
-	}
-
-	glAttachShader(theProgram, theShader);
-}
-
-void CompileShaders()
-{
-	shader = glCreateProgram();
-	if (!shader)
-	{
-		printf("Error creating shader program!");
-		return;
-	}
-
-	AddShader(shader, vShader, GL_VERTEX_SHADER);
-	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
-
-	// Error checking
-	GLint result = 0;
-	GLchar eLog[1024] = { 0 };
-
-	glLinkProgram(shader);
-	glGetProgramiv(shader, GL_LINK_STATUS, &result);
-	if (!result)
-	{
-		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
-		printf(" Error linking program: '%s'\n", eLog);
-		return;
-	}
-
-	glValidateProgram(shader);
-	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
-	if (!result)
-	{
-		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
-		printf(" Error validating program: '%s'\n", eLog);
-		return;
-	}
+	unsigned int shader = CreateShader(vShader, fShader);
+	glUseProgram(shader);
 }
 
 int main()
@@ -154,8 +134,6 @@ int main()
 	// Set context for GLEW to use
 	glfwMakeContextCurrent(mainWindow);
 
-	std::cout << glGetString(GL_VERSION) << std::endl;
-
 	// Allow modern extension features
 	glewExperimental = GL_TRUE;
 
@@ -172,28 +150,23 @@ int main()
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
 	CreateTriangle();
-	CompileShaders();
 
+	// Main loop until user closes the window
 	while (!glfwWindowShouldClose(mainWindow))
 	{
-		// Get and handle user input events
-		glfwPollEvents();
 
 		// Clear window
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Using shader in here
-		glUseProgram(shader);
-		glBindVertexArray(VAO);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glBindVertexArray(0);
-		glUseProgram(0);
-
 		glfwSwapBuffers(mainWindow);
+
+		// Get and handle user input events
+		glfwPollEvents();
 	}
 
+	glDeleteProgram(shader);
+	glfwTerminate();
 	return 0;
 }
